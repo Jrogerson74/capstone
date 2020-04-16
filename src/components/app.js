d7e3b1fbbc41acead68f61bb2759c3c0b2ada713
Jrogerson74 <jrsquish@gmail.com>
2020-04-16 14:41:52 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>HORRIFIED</h1>
+      </div>
+    );
+  }
+}
